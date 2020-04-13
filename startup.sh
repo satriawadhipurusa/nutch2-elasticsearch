d71ee29c790e641564a32f7114278212e1e162ff
Satria Wicaksana Adhipurusa <satriawicaksanaadhipurusa@gmail.com>
2020-04-13 18:48:20 +0700

    tidy up

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./hbase/bin/start-hbase.sh
 
-./$NUTCH_HOME/bin/nutch nutchserver > /dev/null &
-./$NUTCH_HOME/bin/nutch webapp
+$NUTCH_HOME/bin/nutch nutchserver > /dev/null &
+$NUTCH_HOME/bin/nutch webapp
 
